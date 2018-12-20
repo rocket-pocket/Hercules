081dbed37499a840a1ed10d65409300667a3ffbe
daniel <verdudaniel7@gmail.com>
2018-12-20 08:24:19 +0100

    funcionalidad añadida Seccion de login

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 export class Main{
     constructor(){
-        
+        this.btnComenzar = document.querySelector('.btn-comenzar')
+        this.LoginSection = document.querySelector('.login-wrapper-hide')
+
+        this.btnComenzar.addEventListener('click', this.mostrarLoginSection.bind(this))
+    }
+
+    mostrarLoginSection(){
+        this.LoginSection.classList.toggle('login-wrapper-hide')
+        this.btnComenzar.innerHTML = "Lets go!"
     }
 }
