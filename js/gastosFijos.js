3d910e35ff55a8432e2c6194f21898cf8307f299
daniel <verdudaniel7@gmail.com>
2018-12-28 09:12:40 +0100

    estilo parrafo total y funcionalidad sumar resultados gastosF

--- js/gastosFijos.js
+++ js/gastosFijos.js
@@ -3,20 +3,33 @@ export class gastosFijos{
         this.aInputGastosFijos = document.querySelectorAll('.uno input')
         this.btnSumarTotal = document.querySelector('.btn-sumar-total')
         this.outputResultado = document.querySelector('.resultado')
+        this.aBtnBorrarGastoFijo = document.querySelectorAll('.btn-borrar')
+        this.aCampoGastoFijo = document.querySelectorAll('.delete-gasto-fijo')
 console.log(this.outputResultado)
-        this.btnSumarTotal.addEventListener('click', this.sumarTotalInputs.bind(this))
+        this.btnSumarTotal.addEventListener('click', this.sumarTotalGastosFijos.bind(this))
+
+        for (let index = 0; index < this.aBtnBorrarGastoFijo.length; index++) {
+            this.aBtnBorrarGastoFijo[index].addEventListener('click', this.borrarGastoFijo.bind(this))
+        }
+        console.log(this.aBtnBorrarGastoFijo)
     }
-    sumarTotalInputs(){
-        let values = []
-        for (let i = 0; i < this.aInputGastosFijos.length; i++) {
-            /* const element = array[i]; */
-            values.push(this.aInputGastosFijos[i].value)           
-        }  
-        console.log(values)
-        let resultado = values
-        this.outputResultado.innerHTML = `<p>El resultado es: </p> ${resultado}`      
-       
+    sumarTotalGastosFijos() {
+        let total = 0;
+            for (let i = 0; i < this.aInputGastosFijos.length; ++i) {
+                if (!isNaN(parseInt(this.aInputGastosFijos[i].value)))
+                    total += parseFloat(this.aInputGastosFijos[i].value);
+            }
+            let totalizar = this.outputResultado.value = total
+            this.outputResultado.innerHTML = `<p class= "p-total">El total de tus gastos es: ${totalizar}€ </p>`
+            console.log(totalizar)         
     }
+    borrarGastoFijo(){
+        for (let index = 0; index < this.aCampoGastoFijo.length; index++) {
+            /* const element = array[index]; */
+                        
+        }
+    }
+    
     
 }
 
